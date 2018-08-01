685a97a3ec2ee889a354c7c0388a462995bad0eb
Ankush Gupta <no.1ankush@gmail.com>
2018-08-01 12:23:24 -0500

    Added Middleware & Reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 //Library imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 //Component imports
 import App from './components/app';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={ createStoreWithMiddleware(reducers) }>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/reducer-weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      //Concat the new data on the existing state
+      return [ action.payload.data, ...state ];
+  }
+  return state;
+}
